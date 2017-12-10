ab81ed4232bb6f79ce77f2dbe577176cb8fa166e
lisa_eriksson <lisa.c.eriksson@gmail.com>
2017-12-10 15:26:50 +0100

    Adds multiple stories in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,26 @@ chapters = [{"parent_chapter": "", "title": "The story begins", "content": "Ther
  {"parent_chapter": "Shoot it with a laser", "title": "Run", "content": "The monster runs after you. Go right or left?"},
  {"parent_chapter": "Run", "title": "Right", "content": "The monster got stuck and you got away. The end"}]
 
+ [{"parent_chapter": "", "title": "The story begins", "content": "There is a huge asteroid coming your way, what do you do?"},
+  {"parent_chapter": "The story begins", "title": "Shoot it with a laser", "content": "Now a monster emerges from inside the asteroid, wyd?"},
+  {"parent_chapter": "The story begins", "title": "Hide", "content": "The asteroid hits the starship, wyd?"},
+  {"parent_chapter": "Shoot it with a laser", "title": "Talk", "content": "The monster says it wants to kill you, wyd?"},
+  {"parent_chapter": "Shoot it with a laser", "title": "Throw a grenade", "content": "The monster explodes, the whole room is now messy, wyd?"},
+  {"parent_chapter": "Shoot it with a laser", "title": "Run", "content": "The monster runs after you. Go right or left?"},
+  {"parent_chapter": "Run", "title": "Right", "content": "The monster got stuck and you got away. The end"}]
+
+  [{"parent_chapter": "", "title": "The story begins", "content": "There is a huge asteroid coming your way, what do you do?"},
+   {"parent_chapter": "The story begins", "title": "Shoot it with a laser", "content": "Now a monster emerges from inside the asteroid, wyd?"},
+   {"parent_chapter": "The story begins", "title": "Hide", "content": "The asteroid hits the starship, wyd?"},
+   {"parent_chapter": "Shoot it with a laser", "title": "Talk", "content": "The monster says it wants to kill you, wyd?"},
+   {"parent_chapter": "Shoot it with a laser", "title": "Throw a grenade", "content": "The monster explodes, the whole room is now messy, wyd?"},
+   {"parent_chapter": "Shoot it with a laser", "title": "Run", "content": "The monster runs after you. Go right or left?"},
+   {"parent_chapter": "Run", "title": "Right", "content": "The monster got stuck and you got away. The end"}]
+
+
 stories = [{"title": "Starship", "author": "Mariza & Alfred", "intro": "Once upon a time, in a far away galaxy. There was a young team on the Discovery Starship"}]
+          [{"title": "Starship 2", "author": "Mariza & Alfred", "intro": "Once upon a time, in a far away galaxy. There was a young team on the Discovery Starship"}]
+          [{"title": "Starship 3", "author": "Mariza & Alfred", "intro": "Once upon a time, in a far away galaxy. There was a young team on the Discovery Starship"}]
 
 stories.each do |hash|
   Story.create(hash)
